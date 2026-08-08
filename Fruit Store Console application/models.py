ff3664dc43956828c5ffed887064ef58ae84c3eb
Niyati Gajjar <niyatigajjar03@gmail.com>
2026-08-08 20:50:40 +0530

    Add files via upload

---
+++ Fruit Store Console application/models.py
@@ -0,0 +1,21 @@
+# Models module for storing data structures and basic operations
+# This module defines the core data structure for fruits in the system
+
+class Fruit:
+    """Class to represent a fruit item with its properties"""
+    def __init__(self, name, quantity, price):
+        # Initialize fruit properties
+        self.name = name          
+        self.quantity = quantity
+        self.price = price   
+
+    def to_dict(self):
+        """Convert fruit object to dictionary for storage"""
+        return {
+            'name': self.name,
+            'quantity': self.quantity,
+            'price': self.price
+        }
+    def from_dict(cls, data):
+        """Create fruit object from dictionary data"""
+        return cls(data['name'], data['quantity'], data['price'])
